5b6408a3f27a66f931ec7806c6c41d0ac7f36160
tobbe1111 <tobias.west@ha.ax>
2024-09-03 19:03:15 +0300

    started new project

---
+++ src/main/java/it/ha/ax/laboration2website/Laboration2WebsiteApplication.java
@@ -0,0 +1,13 @@
+package it.ha.ax.laboration2website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboration2WebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboration2WebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/ha/ax/laboration2website/Laboration2WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ha.ax.laboration2website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboration2WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
